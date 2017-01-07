1aec09537fa2d5350021d31e3e597e83d94e5578
Bertrand Benoit <contact@bertrand-benoit.net>
2017-01-07 18:17:08 +0100

    Removed all source code linked to steam-api which is no more used.

--- mysteamclient.js
+++ mysteamclient.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
 const util = require('util');
-
-// const testWithSteamApi = require(__dirname + '/lib/testWithSteamApi.js');
 const testWithSteamWeb = require(__dirname + '/lib/testWithSteamWeb.js');
 
 var mySteamID = '76561198046029799';
-var appId = 377160; // Fallout 4
-
-// testWithSteamApi.allTests(mySteamID, appId, function() {
-//   console.log('OK');
-// });
 
 testWithSteamWeb.showAllInterestingStats(mySteamID, function(error, result) {
   if (error)
