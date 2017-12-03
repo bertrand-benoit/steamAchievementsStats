20d9ca7db74034f6fa8e87a0db05f7ee58bbd70d
Bertrand Benoit <contact@bertrand-benoit.net>
2017-12-03 12:01:06 +0100

    Implemented a 'forced stats' system allowing to be closer to Steam computation.

--- steam-cli.js
+++ steam-cli.js
@@ -11,3 +11,6 @@ testWithSteamWeb.showAllInterestingStats(mySteamID, function(error, result) {
 
   console.log(util.inspect(result, false, null, true));
 });
+
+
+//http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=C281B1657755D9A5DD02D84608BCB6AB&steamid=76561198046029799&format=json&include_appinfo=1&include_played_free_games=1
